d6272d0be2948ad125fd2195e316c8280e4bd628
DLindeblom <drdlindeblom@gmail.com>
2022-06-08 19:11:07 +0000

    tested the move command

--- client.js
+++ client.js
@@ -13,6 +13,15 @@ const connect = function () {
     console.log(data);  
   });
 
+  const name = "DRL";
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server")
+    conn.write(`Name: ${name}`)
+    // setInterval(() => {
+    //   conn.write("Move: up")
+    // }, 500)
+  })
+
   return conn;
 };
 
