d9eb6485d5b5cf17fdf36f0cde48b056e4c5951e
DLindeblom <drdlindeblom@gmail.com>
2022-06-08 19:35:44 +0000

    refactor play and add input

--- play.js
+++ play.js
@@ -1,21 +1,2 @@
-const net = require("net")
-const connect = require("./client")
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-
-setupInput()
+const { connect } = require("./client")
+const { setupInput } = require('./input')
